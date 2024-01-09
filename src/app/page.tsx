93e2d152d17f867a7edda9f4e2230ddd38ade222
Alex <aleksander.misterkiewicz@outlook.com>
2024-01-09 10:48:30 +0100

    doc page footer change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,15 +112,14 @@ export default function Home() {
           </div>
 
           <p className={styles.footer}>
-            {targetCompany && (
-              <>
-                I hereby give consent for my personal data to be processed by{" "}
-                {targetCompany} for the purpose of conducting recruitment for
-                the position for which I am applying. I also consent to
-                processing of my personal data by {targetCompany} for the
-                purposes of any future recruitment processes.
-              </>
-            )}
+            I agree to the processing of personal data provided in this document
+            for realising the recruitment process pursuant to the Personal Data
+            Protection Act of 10 May 2018 (Journal of Laws 2018, item 1000) and
+            in agreement with Regulation (EU) 2016/679 of the European
+            Parliament and of the Council of 27 April 2016 on the protection of
+            natural persons with regard to the processing of personal data and
+            on the free movement of such data, and repealing Directive 95/46/EC
+            (General Data Protection Regulation).
           </p>
         </div>
       </main>
